16b41c8ec62bfd5a32237561e9a8268fa660f3b3
ViniBelo <vinialvesbelo@outlook.com>
2025-09-22 17:32:38 -0300

    Initial commit

---
+++ src/main/java/br/com/vinibelo/githubprofilemanagermutant/GitHubProfileManagerMutantApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinibelo.githubprofilemanagermutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProfileManagerMutantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubProfileManagerMutantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/vinibelo/githubprofilemanagermutant/GitHubProfileManagerMutantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinibelo.githubprofilemanagermutant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubProfileManagerMutantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
